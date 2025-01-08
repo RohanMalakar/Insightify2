29d9f46371caba08ff8d8f3794d04022399a2c54
RohanMalakar <rohanmalakar5091>
2025-01-09 04:09:29 +0530

    addedorigin to cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,11 @@ import langflowRoutes from "./routes/LangFlow.route.js";
 const app = express();
 const _dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://insightify2-phi.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use("/api/v1/langflow", langflowRoutes); // Mount the Langflow routes
